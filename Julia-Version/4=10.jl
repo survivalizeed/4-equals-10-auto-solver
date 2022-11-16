3bd023e49c8fef0da98152e65a7bb00012b44086
Gero Bodemann <78881613+survivalizeed@users.noreply.github.com>
2022-11-16 21:03:28 +0100

    Now completely working version!

--- Julia-Version/4=10.jl
+++ Julia-Version/4=10.jl
@@ -1,10 +1,43 @@
 errMessage() = println("Invalid Number! Try a number between -9 and 9 incl.")
 clear() = run(`cmd /c cls`)
-decide(index) = (index == 0 && return "+"; index == 1 && return "-"; index == 2 && return "*"; index == 3 && return "/")
+function decide(index) 
+    c = 0
+    # if a symbol is excluded, find one that isn't
+    while c < 4
+        if index == 0 
+            if exclude["+"]
+                index += 1
+                c += 1
+            else break end
+        end
+        if index == 1 
+            if exclude["-"]
+                index += 1
+                c += 1
+            else break end
+        end
+        if index == 2 
+            if exclude["*"]
+                index += 1
+                c += 1
+            else break end
+        end
+        if index == 3
+            if exclude["/"]
+                index += 1
+                c += 1
+            else break end
+        end
+    end
+    c == 4 && (println("Too much symbols excluded"); exit();)
+    (index == 0 && return "+"; index == 1 && return "-"; index == 2 && return "*"; index == 3 && return "/")
+end
+
 function input(index::Int8)
     while true
         println("Input number: ", index)
         input = readline()
+        # message variable to prevent double error message
         message = false
         try
             -9 <= parse(Int8, input) <= 9 && return parse(Int8, input)
@@ -12,6 +45,7 @@ function input(index::Int8)
             errMessage()
             message = true
         end
+        # valid runtime but number is out of range 
         !message && errMessage()
         message = false
     end
@@ -27,9 +61,10 @@ function swapper(nums::Matrix{Int8})
         indices[2] += 1
     end
     indices[1] += 1
-    nums
+    return nums
 end
 function bracket(index, id, lb)
+    exclude["b"] && return ""
     if index == id && lb 
         return "("      
     end
@@ -38,32 +73,39 @@ function bracket(index, id, lb)
     end
     return ""
 end
-function procedure()    
+function procedure()   
+    # indices to identify current swap state of the input numbers
     global indices = [1, 1]
     resultCounter = 0
     origNums = zeros(Int8, 1, 4)
     for i::Int8 in 1:4
         origNums[i] = input(i)
     end
+    println("Excluded symbols: b = bracket | + = + | - = - | * = * | / = /\nExample: b+-")
+    excludeStr = readline()
+    # dict which contains if a certain symbol is excluded
+    global exclude = Dict("b" => occursin("b", excludeStr), "+" => occursin("+", excludeStr), "-" => occursin("-", excludeStr), 
+        "*" => occursin("*", excludeStr), "/" => occursin("/", excludeStr))
     while true
         nums = swapper(copy(origNums))
         if isnothing(nums)
             println("Result: No solution found!")
+            sleep(3)
             return
         end
         for ac in 0:3, bc in 0:3, cc in 0:3, lb in 0:2, rb in 1:3     
-            lb == 2 && (rb += 1)
-            rb > 3 && (rb = 3)
+            lb == 2 && (rb += 1)    # lb == left bracket
+            rb > 3 && (rb = 3)      # rb == right bracket 
             expr = string(bracket(lb, 0, true), convert(Float64, nums[1]), " ", 
             decide(ac), " ", bracket(lb, 1, true), convert(Float64, nums[2]), bracket(rb, 1, false), " ", decide(bc), " ",  bracket(lb, 2, true), 
             convert(Float64, nums[3]), bracket(rb, 2, false), " ", decide(cc), " ", convert(Float64, nums[4]), bracket(rb, 3, false))
             if eval(Meta.parse(expr)) == 10.0
                 resultCounter += 1
                 println("Result ", resultCounter, ": ", replace(expr, ".0" => ""))
-                println("Next Solution? Y/N")
+                println("Next Solution? Y/N (Brute Forced)")
                 while true
                     local input = readline()
-                    if input == "y" || input == "Y" 
+                    if input == "y" || input == "Y"  
                         break;
                     elseif input == "n" || input "N"
                         return;
