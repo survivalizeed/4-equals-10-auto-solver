7977f5f3f4ef4b9d5836eb20a9ce08a6afa35453
Gero Bodemann <78881613+survivalizeed@users.noreply.github.com>
2022-11-17 18:33:31 +0100

    Add files via upload

--- Julia-Version/4=10.jl
+++ Julia-Version/4=10.jl
@@ -29,13 +29,12 @@ function decide(index)
             else break end
         end
     end
-    c == 4 && (println("Too much symbols excluded"); exit();)
+    c == 4 && (println("Too much symbols excluded"); return "")
     (index == 0 && return "+"; index == 1 && return "-"; index == 2 && return "*"; index == 3 && return "/")
 end
-
 function input(index::Int8)
     while true
-        println("Input number: ", index)
+        println("Input number ", index, ":")
         input = readline()
         # message variable to prevent double error message
         message = false
@@ -45,7 +44,7 @@ function input(index::Int8)
             errMessage()
             message = true
         end
-        # valid runtime but number is out of range 
+        # valid runtime but number is out of range
         !message && errMessage()
         message = false
     end
@@ -53,9 +52,8 @@ end
 function swapper(nums::Matrix{Int8})
     indices[2] > 4 && return nothing
     if indices[1] <= 4
-        tmp = nums[indices[1]]
-        nums[indices[1]] = nums[indices[2]]
-        nums[indices[2]] = tmp
+        # swap the nums through
+        nums[indices[1]], nums[indices[2]] = nums[indices[2]], nums[indices[1]]
     else
         indices[1] = 0
         indices[2] += 1
@@ -81,24 +79,37 @@ function procedure()
     for i::Int8 in 1:4
         origNums[i] = input(i)
     end
-    println("Excluded symbols: b = bracket | + = + | - = - | * = * | / = /\nExample: b+-")
+    println("Excluded symbols: b = bracket | + = + | - = - | * = * | / = / |  Example: b+-")
     excludeStr = readline()
     # dict which contains if a certain symbol is excluded
     global exclude = Dict("b" => occursin("b", excludeStr), "+" => occursin("+", excludeStr), "-" => occursin("-", excludeStr), 
-        "*" => occursin("*", excludeStr), "/" => occursin("/", excludeStr))
+    "*" => occursin("*", excludeStr), "/" => occursin("/", excludeStr))
+    clear()
     while true
+        # copy to keep the original orde
         nums = swapper(copy(origNums))
         if isnothing(nums)
             println("Result: No solution found!")
             sleep(3)
             return
         end
+        # one-line multi nested loop to iterate over all possible symbol combinations
         for ac in 0:3, bc in 0:3, cc in 0:3, lb in 0:2, rb in 1:3     
             lb == 2 && (rb += 1)    # lb == left bracket
             rb > 3 && (rb = 3)      # rb == right bracket 
+
+            decideA, decideB, decideC = decide(ac), decide(bc), decide(cc)
+            if decideA == "" || decideB == "" || decideC == ""
+                sleep(3)
+                return
+            end
+            # create the expression
+            # decide returns the corresponding mathmatical symbol
+            # decide gets compared to "" to return if too many symbols were excluded (see ret val options of decide)
+            # converting the nums integers to floats for precise comparesion
             expr = string(bracket(lb, 0, true), convert(Float64, nums[1]), " ", 
-            decide(ac), " ", bracket(lb, 1, true), convert(Float64, nums[2]), bracket(rb, 1, false), " ", decide(bc), " ",  bracket(lb, 2, true), 
-            convert(Float64, nums[3]), bracket(rb, 2, false), " ", decide(cc), " ", convert(Float64, nums[4]), bracket(rb, 3, false))
+            decideA, " ", bracket(lb, 1, true), convert(Float64, nums[2]), bracket(rb, 1, false), " ", decideB, " ",  
+            bracket(lb, 2, true), convert(Float64, nums[3]), bracket(rb, 2, false), " ", decideC, " ", convert(Float64, nums[4]), bracket(rb, 3, false))
             if eval(Meta.parse(expr)) == 10.0
                 resultCounter += 1
                 println("Result ", resultCounter, ": ", replace(expr, ".0" => ""))
